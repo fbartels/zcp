a3e913e2965e8aeb78d9a6468ed5262a7ef6ba38
Jan Engelhardt <jengelh@inai.de>
2015-08-27 20:37:32 +0200

    build: remove cruft calling all autotools manually
    There is already a superior tool doing all that for us.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-testrun() {
-	if [ -z "`which $1`" ]; then
-		echo $1 is needed to build
-		false
-	else
-		echo "Running $@"
-		"$@"
-	fi
-	if [ $? -ne 0 ]; then exit; fi
-}
-
-
-if ! pkg-config --version >/dev/null 2>&1 ; then
-	echo 'Please install pkg-config >= 0.18 before running '$0
-	exit 1
-fi
-if test `pkg-config --version` \< "0.18"; then
-	echo 'Please install pkg-config >= 0.18 before running '$0
-	exit 1
-fi
-
-testrun ${AUTOHEADER:-autoheader}
-testrun ${ACLOCAL:-aclocal} -Iautoconf
-testrun ${AUTOCONF:-autoconf}
-testrun ${LIBTOOLIZE:-libtoolize} --copy --automake --force
-testrun ${AUTOMAKE:-automake} --add-missing --copy --foreign --force
-
-echo
-echo 'Please run ./configure && make'
-#./configure
-#make
+exec autoreconf -fiv
