93bd89ffab4f17e175dfdf1d887c2b9b68c74f02
Mark Dufour <m.dufour@zarafa.com>
2015-11-24 11:58:21 +0100

    pyza: scripts/tree.py: add outer loop for companies

--- swig/python/zarafa/scripts/tree.py
+++ swig/python/zarafa/scripts/tree.py
@@ -7,12 +7,14 @@
 import zarafa
 
 server = zarafa.Server()
-for user in server.users(): # checks -u/--user command-line option
-     print user
-     for folder in user.store.folders():
-         indent = (folder.depth+1)*'  '
-         print indent + unicode(folder)
-         for item in folder:
-             print indent + '  ' + unicode(item)
-             for attachment in item.attachments():
-                 print indent + '    ' + attachment.filename 
+for company in server.companies(): # checks -c/--company commond-line option
+    print company
+    for user in company.users(): # checks -u/--user command-line option
+         print user
+         for folder in user.store.folders():
+             indent = (folder.depth+1)*'  '
+             print indent + unicode(folder)
+             for item in folder:
+                 print indent + '  ' + unicode(item)
+                 for attachment in item.attachments():
+                     print indent + '    ' + attachment.filename
