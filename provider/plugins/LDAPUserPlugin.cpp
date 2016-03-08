d49d1a25a1a57ced1b51e4fe4fe0eda4b1f27f49
Jan Engelhardt <jengelh@inai.de>
2016-03-08 21:28:28 +0100

    plugins/ldap: avoid crash when bad server URI used
    ldap_initialize() can fail if a syntactically bad hostname was
    entered. (For example, set "ldap_host = localhost ohsnap" in
    /etc/zarafa/ldap.cfg.) When this happens, no LDAP object is
    constructed, leaving the "ld" variable with a NULL value.
    ldap_unbind_s() must subsequently _not_ be called, because it does
    not like NULL pointers.
    
    (gdb)
        "ld != ((void *)0)", file=0x7fb797cd6551 "unbind.c", line=45,
        function=0x7fb797cd6580 "ldap_unbind_ext") at assert.c:101
        bind_dn=0x1f65400 "cn=root,o=da", bind_pw=0x1f65800 "zadagent") at
        LDAPUserPlugin.cpp:575
    
    References: ZCP-13623

--- provider/plugins/LDAPUserPlugin.cpp
+++ provider/plugins/LDAPUserPlugin.cpp
@@ -511,7 +511,7 @@ LDAP *LDAPUserPlugin::ConnectLDAP(const char *bind_dn, const char *bind_pw) {
 			m_lpStatsCollector->Increment(SCN_LDAP_CONNECT_FAILED);
 
 			m_logger->Log(EC_LOGLEVEL_FATAL, "Failed to initialize LDAP for %s: %s", currentServer.c_str(), ldap_err2string(rc));
-			goto fail;
+			goto fail2;
 		}
 
 		m_logger->Log(EC_LOGLEVEL_DEBUG, "Trying to connect to %s", currentServer.c_str());
@@ -567,14 +567,13 @@ LDAP *LDAPUserPlugin::ConnectLDAP(const char *bind_dn, const char *bind_pw) {
 		m_logger->Log(EC_LOGLEVEL_WARNING, "LDAP (simple-) bind failed: %s", ldap_err2string(rc));
 
 	fail:
+		if (ldap_unbind_s(ld) == -1)
+			m_logger->Log(EC_LOGLEVEL_ERROR, "LDAP unbind failed");
+	fail2:
 		// see if another (if any) server does work
 		ldapServerIndex++;
 		if (ldapServerIndex >= ldap_servers.size())
 			ldapServerIndex = 0;
-
-		if (ldap_unbind_s(ld) == -1)
-			m_logger->Log(EC_LOGLEVEL_ERROR, "LDAP unbind failed");
-
 		m_lpStatsCollector->Increment(SCN_LDAP_CONNECT_FAILED);
 
 		ld = NULL;
