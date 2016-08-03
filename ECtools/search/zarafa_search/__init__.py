6186c89e06278112b9e36666c6a0eaa7a70bed9a
Mark Dufour <m.dufour@zarafa.com>
2016-08-03 20:19:51 +0200

    search: replace sleep for SYNCRUN command with multiprocessing value
    Tests may issue a SYNCRUN command to search, to wait for indexing to
    finish. Of course, using a sleep for this is suboptimal, so replace it
    with a proper multiprocessing solution.
    
    References: KC-46

--- ECtools/search/zarafa_search/__init__.py
+++ ECtools/search/zarafa_search/__init__.py
@@ -4,7 +4,7 @@ import dbhash
 import fcntl
 import os.path
 from Queue import Empty
-from multiprocessing import Queue
+from multiprocessing import Queue, Value
 import time
 import sys
 
@@ -101,8 +101,10 @@ class SearchWorker(zarafa.Worker):
                     if cmd == 'PROPS':
                         response(conn, 'OK:'+' '.join(map(str, config['index_exclude_properties'])))
                         break
-                    if cmd == 'SYNCRUN': # wait for syncing to be up-to-date (used from testset)
-                        time.sleep(10) # XXX use multiprocessing signal
+                    if cmd == 'SYNCRUN': # wait for syncing to be up-to-date (only used in tests)
+                        self.syncrun.value = time.time()
+                        while self.syncrun.value:
+                            time.sleep(1)
                         response(conn, 'OK:')
                         break
                     elif cmd == 'SCOPE':
@@ -278,7 +280,8 @@ class Service(zarafa.Service):
             self.state = new_state
             db_put(self.state_db, 'SERVER', self.state)
             self.log.info('saved server sync state = %s' % self.state)
-        SearchWorker(self, 'query', reindex_queue=self.reindex_queue).start()
+        self.syncrun = Value('d', 0)
+        SearchWorker(self, 'query', reindex_queue=self.reindex_queue, syncrun=self.syncrun).start()
         self.log.info('starting incremental sync')
         self.incremental_sync()
 
@@ -319,6 +322,8 @@ class Service(zarafa.Service):
                     self.state = new_state
                     db_put(self.state_db, 'SERVER', self.state)
                     self.log.info('saved server sync state = %s' % self.state)
+                if t0 > self.syncrun.value+1:
+                    self.syncrun.value = 0
             time.sleep(5)
 
     def reindex(self):
